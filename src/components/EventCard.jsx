e73182bc642d45d8e4ead0e7c0fbb6a907d0dcd3
Gregory0401 <grigoriosdart@gmail.com>
2023-08-02 16:31:56 +0300

    main

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -28,21 +28,27 @@ function EventCard({ task, detailsEvent }) {
 
   return (
     <ul className={s.card}>
-      <li className={s.category}>{task.category}</li>
-      {task.priority === 'height' && <li className={s.p_height}>Hight</li>}
-      {task.priority === 'medium' && <li className={s.p_medium}>Medium</li>}
-      {task.priority === 'low' && <li className={s.p_low}>Low</li>}
-      {task.category === 'art' && <li className={s.art}></li>}
-      {task.category === 'business' && <li className={s.business}></li>}
-      {task.category === 'party' && <li className={s.party}></li>}
-      {task.category === 'music' && <li className={s.music}></li>}
+      <div className={s.image}>
+        {task.category === 'art' && <li className={s.art}></li>}
+        {task.category === 'business' && <li className={s.business}></li>}
+        {task.category === 'party' && <li className={s.party}></li>}
+        {task.category === 'music' && <li className={s.music}></li>}
+      </div>
+      <div className={s.info}>
+        <li className={s.category}>{task.category}</li>
+        <li className={s.priority}>
+          {task.priority === 'high' && <li className={s.high}>high</li>}
+          {task.priority === 'medium' && <li className={s.medium}>medium</li>}
+          {task.priority === 'low' && <li className={s.low}>low</li>}
+        </li>
+      </div>
       <li className={s.item}>{task.date}</li>
       <li className={s.title}>{task.name}</li>
-      <li className={s.title}>{task.description}</li>
+      <li className={s.description}>{task.description}</li>
       <NavLink to="/details" className={s.link}>
         <button
           type="button"
-          className={s.title}
+          className={s.button}
           onClick={() => {
             detailsEvent(task.id);
           }}
